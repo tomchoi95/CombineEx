7acd9905209712c56fdc7603bedda3567c606d0b
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-02 22:12:30 +0900

    Initial Commit

---
+++ CombineEx/CombineExApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineExApp.swift
+//  CombineEx
+//
+//  Created by 최범수 on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct CombineExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineEx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineEx
+//
+//  Created by 최범수 on 2025-04-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
