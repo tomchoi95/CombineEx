0f3b37244b2c578729c6bc78abdc94bac3982653
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-03 08:37:10 +0900

    remove duplicate function usage

--- CombineEx/MyPlayground.playground/Contents.swift
+++ CombineEx/MyPlayground.playground/Contents.swift
@@ -1,6 +1,14 @@
 import Combine
 
-let pizzaToppings: Publishers.Sequence<[String], Never> = ["Peperoni", "Mushrooms", "Onions", "Salami", "Bacon", "Extra cheese", "Black olives", "Green peppers"].publisher
+let pizzaToppings: Publishers.Sequence<[String], Never> = ["Peperoni", "Mushrooms", "Mushrooms","Mushrooms", "Onions", "Salami", "Bacon", "Extra cheese", "Black olives", "Green peppers"].publisher
 
 // sequence를 publisher로 만들음.
 // 타입은 Publishers.Sequence<[String], Never>임.
+// 이걸 구독하고, 데이터를 받아서 print해 보자.
+
+pizzaToppings
+    .removeDuplicates()
+    .sink { print($0) }
+
+// sink는 데이터를 구독하는 함수. 
+// removeDuplicates 연산자?는 연속된 값? 데이터?를 무시할 수 있음.
