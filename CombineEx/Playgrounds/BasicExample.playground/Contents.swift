f2ff147445f0d049d38c9050b7d457df00a4f58d
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-04 16:21:16 +0900

    Just로 데이터 전달하기.

--- CombineEx/Playgrounds/BasicExample.playground/Contents.swift
+++ CombineEx/Playgrounds/BasicExample.playground/Contents.swift
@@ -4,3 +4,14 @@ import Combine
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+var cancellables: Set<AnyCancellable> = []
+// 하나의 값만 발행하는 publisher
+let disposablePublisher = Just("Hi There!")
+//    .eraseToAnyPublisher()
+// 이 publisher는 무조건 성공울 보장하고, 성공값을 보여주는 것 같다.
+// 그렇다면 이 publisher를 구독해 보자.
+
+disposablePublisher.sink { str in
+    print("I received value of \(str)")
+}.store(in: &cancellables)
+
