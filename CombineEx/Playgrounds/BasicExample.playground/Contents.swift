36b7ad266a3c032f911bb1d392da9f4b127f6cc8
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-04 17:00:49 +0900

    이로써, Just와 Fail을 써볼 수 있었음.

--- CombineEx/Playgrounds/BasicExample.playground/Contents.swift
+++ CombineEx/Playgrounds/BasicExample.playground/Contents.swift
@@ -21,3 +21,25 @@ disposablePublisher.sink { completion in
 } receiveValue: { input in
     print(input)
 }.store(in: &cancellables)
+// 갑자기 궁금해짐. cancellables을 print해 보자.
+
+
+
+enum TomError: Error {
+    case somethingWrong
+}
+
+// 그럼 무조건 실패하는 경우도 있겠네? 무조건 실패를 해 보자.
+let verifiedFailPublisher = Fail(outputType: TomError.self, failure: TomError.somethingWrong).eraseToAnyPublisher()
+
+verifiedFailPublisher.sink { completion in
+    switch completion {
+        case .finished :
+            print("It will never happen, maybe?")
+        case .failure(let error):
+            print(error)
+    }
+} receiveValue: { sth in
+    print(sth)
+}.store(in: &cancellables)
+
