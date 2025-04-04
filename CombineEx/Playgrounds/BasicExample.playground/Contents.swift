a0c0e32267715830a7935cb5105c5c7c784e41fb
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-04 16:40:30 +0900

    Just 동작 확인 완료.

--- CombineEx/Playgrounds/BasicExample.playground/Contents.swift
+++ CombineEx/Playgrounds/BasicExample.playground/Contents.swift
@@ -11,7 +11,13 @@ let disposablePublisher = Just("Hi There!")
 // 이 publisher는 무조건 성공울 보장하고, 성공값을 보여주는 것 같다.
 // 그렇다면 이 publisher를 구독해 보자.
 
-disposablePublisher.sink { str in
-    print("I received value of \(str)")
+disposablePublisher.sink { completion in
+    switch completion {
+        case .finished:
+            print("finished")
+        case .failure(let e):
+            print("I never fail, that's how i set")
+    }
+} receiveValue: { input in
+    print(input)
 }.store(in: &cancellables)
-
