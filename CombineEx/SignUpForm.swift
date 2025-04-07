845360d4d99a64bb5bc1a489dac2a0d3a939bea5
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-07 17:04:10 +0900

    view complete.. not yet actually

--- CombineEx/SignUpForm.swift
+++ CombineEx/SignUpForm.swift
@@ -9,14 +9,45 @@ import SwiftUI
 import Combine
 
 
+
 struct SignUpForm: View {
     
+    @Binding var willBeBinded: String
+    
     var body: some View {
-        
-        
+        Form {
+            idSection
+            passwordSection
+            confirmButton
+        }
     }
+    
+    private var idSection: some View {
+        Section {
+            TextField("Username", text: $willBeBinded)
+                .autocorrectionDisabled()
+        } footer: {
+            Text(verbatim: "error")
+                .foregroundStyle(.red)
+        }
+    }
+    private var passwordSection: some View {
+        Section {
+            SecureField("Password", text: $willBeBinded)
+            SecureField("Password Confirmation", text: $willBeBinded)
+        }
+    }
+    private var confirmButton: some View {
+        Section {
+            Button("Sin in?") {
+                
+            }
+//            .disabled(<#T##disabled: Bool##Bool#>)
+        }
+    }
+    
 }
 
 #Preview {
-    SignUpForm()
+    SignUpForm(willBeBinded: .constant(""))
 }
